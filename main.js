489113b724ca278ea3506d109143f52ef0509af3
Yegorich75 <yegorich75@gmail.com>
2019-06-18 08:53:44 +0300

    scroll percent

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const progress = document.querySelector('.progress');
 window.addEventListener('scroll', progressBar);
 
 function progressBar(event) {
-    console.log('Scroll');
-
+    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let per = windowScroll / windowHeight *100;
+    
+    progress.style.width = per + '%';
 }
